03997e82623a64a27ae4edd5c0df175c2d7d90b8
lzy1102 <575566430@qq.com>
2022-01-12 09:55:24 +0800

    filter ip

--- plugin/dbsave/main.go
+++ plugin/dbsave/main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"go.mongodb.org/mongo-driver/bson"
 	"io/ioutil"
-	"os"
 	"searchproxy/fram/config"
 	db2 "searchproxy/fram/db"
 	"searchproxy/fram/utils"
-	"time"
 )
 
 type save struct {
@@ -26,34 +23,33 @@ func init() {
 }
 
 func main() {
-	var dbcfg db2.MongoConfig
-	config.Install().Get("mongo", &dbcfg)
-	sc.db = db2.NewMongo(&dbcfg)
-
-	var cachecfg db2.RedisConfig
-	config.Install().Get("cache", &cachecfg)
-	sc.cache = db2.NewRedis(&cachecfg)
-
 	bts, err := ioutil.ReadFile(sc.datafile)
 	utils.FatalAssert(err)
 	var data map[string]interface{}
 	utils.FatalAssert(json.Unmarshal(bts, &data))
 
-	if ipstr, ok := data["ip"]; ok {
-		data["ip"] = utils.Ip2Int64(ipstr.(string))
-	}
-	var result map[string]interface{}
-	_ = sc.db.FindOne("info", bson.M{"ip": data["ip"], "port": data["port"]}, &result)
-	if id, ok := result["_id"]; ok {
-		_, _ = sc.db.UpdateOne("info", bson.M{"_id": id}, data)
-	} else {
-		_, err = sc.db.InsertOne("info", data)
-		if err == nil {
-			get := sc.cache.GetInt64("proxycount")
-			sc.cache.Set("proxycount", get+1, time.Hour*1000000)
-		}
-	}
-	os.RemoveAll(sc.datafile)
-	sc.db.Close()
-	sc.cache.Close()
+	var dbcfg db2.MongoConfig
+	config.Install().Get("mongo", &dbcfg)
+	sc.db = db2.NewMongo(&dbcfg)
+
+	//var cachecfg db2.RedisConfig
+	//config.Install().Get("cache", &cachecfg)
+	//sc.cache = db2.NewRedis(&cachecfg)
+	//if ipstr, ok := data["ip"]; ok {
+	//	data["ip"] = utils.Ip2Int64(ipstr.(string))
+	//}
+	//var result map[string]interface{}
+	//_ = sc.db.FindOne("info", bson.M{"ip": data["ip"], "port": data["port"]}, &result)
+	//if id, ok := result["_id"]; ok {
+	//	_, _ = sc.db.UpdateOne("info", bson.M{"_id": id}, data)
+	//} else {
+	//	_, err = sc.db.InsertOne("info", data)
+	//	if err == nil {
+	//		get := sc.cache.GetInt64("proxycount")
+	//		sc.cache.Set("proxycount", get+1, time.Hour*1000000)
+	//	}
+	//}
+	//os.RemoveAll(sc.datafile)
+	//sc.db.Close()
+	//sc.cache.Close()
 }
