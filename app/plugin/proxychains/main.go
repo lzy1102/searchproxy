e0ab60437cd8611f0aa33ae26fc4278128d331c3
lzy1102 <575566430@qq.com>
2022-01-26 15:04:14 +0800

    s

---
+++ app/plugin/proxychains/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/imroc/req"
+	"log"
+)
+
+func main() {
+	resp, err := req.Get("http://www.google.com")
+	if err != nil {
+		return
+	}
+	log.Println(resp.String())
+}
